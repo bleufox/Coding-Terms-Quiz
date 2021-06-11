95c39dcd08ae8c355c344621ac78267e46429384
bleufox <thebleufox8@gmail.com>
2021-06-11 13:49:44 -0700

    Upload for Wyzant Tutoring Session

--- js/script.js
+++ js/script.js
@@ -1,51 +1,78 @@
-const startBtn = document.getElementById('start-button');
-const startScreen = document.getElementById('start-screen');
-const quizBox = document.getElementById('questions');
-const timeLeft = 60;
-const score = 0;
-const listOfQuestions = [{
+const startButton = document.getElementById('start-btn');
+const questionContainerEl = document.getElementById('question-container');
+const startScreenEl = document.getElementById('start-screen');
+const hsButton = document.getElementById('hs-btn');
+const timerEl = document.getElementById('timer');
+let shuffledQuestions, currentQuestionIndex
+const questionsEl = document.getElementById('question');
+const answerBtnsEl = document.getElementById('answer-buttons')
+startButton.addEventListener('click', startQuiz);
+
+const questions = [{
 
     question: "What does the acronym HTML stand for?",
-    answers: ["Hey Too Much Layout", "Hypertext Markup Language", "Hey There Mother Lord", "Hang (on), There's More (to) Learn"],
-    correctAns: "Hypertext Markup Language",
+    answers: [
+        { text: "Hey Too Much Layout", correct: false },
+        { text: "Hypertext Markup Language", correct: true },
+        { text: "Hey There Mother Lord", correct: false },
+        { text: "Hang (on), There's More (to) Learn", correct: false }
+    ]
 },
+
 {
     question: "What is Javascript?",
-    answers: ["A script for a movie about a coffee shop", "A type of food", "A brand name", "An object-oriented computer programming language"],
-    correctAns: "An object-oriented computer programming language",
+    answers: [
+        { text: "A script for a movie about a coffee shop", correct: false },
+        { text: "A type of food", correct: false },
+        { text: "A brand name", correct: false },
+        { text: "An object-oriented computer programming language", correct: true },
+    ]
 },
 {
     question: "'Section', 'Article', 'Nav', and 'Footer' are examples of:",
-    answers: ["HTML Semantic Elements", "Variables", "Properties", "Absolute Nonsense"],
-    correctAns: "HTML Semantic Elements"
-},
-{
-    question: "What is CSS used for?",
-    answers: ["It is used to style a DOM", "It's not used in coding", "I have no idea", "It is strictly used for mathematical calculations"],
-    correctAns: "It is used to style a DOM",
-}]
+    answers: [
+        { text: "HTML Elements", correct: true },
+        { text: "Variables", correct: false },
+        { text: "Properties", correct: false },
+        { text: "Absolute Nonsense", correct: false }
+    ]
+}],
 
 function startQuiz() {
-    startScreen.style.display = "none"
-    quizBox.style.display = "block"
+    console.log("Started")
+    startButton.classList.add('hide')
+    hsButton.classList.add('hide')
+    startScreenEl.classList.add('hide')
+    timerEl.classList.remove('hide')
+    questionContainerEl.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
 }
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(questions) {
+    questionContainerEl.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+
 // to render high scores
 // get local storage and put it in a array
 // push the new high score into that array
 // set the new array into local storage
 
-function questions() {
-    startScreen.style.display = "none"
-    quizBox.style.display = "block"
-}
+// function countDown() {
+//     timeLeft--;
+//     document.getElementById("clock").textContent = timeLeft;
 
-function countDown() {
-    timeLeft--;
-    document.getElementById("clock").textContent = timeLeft;
+//     if (timeLeft <= 0) {
 
-    if (timeLeft <= 0) {
-
-    }
-}
-
-startBtn.onclick = startQuiz
+//     }
+// }
